864e23fa3de2fd65d35e5c5b8d01dd53efa529ee
levarock <60803180+levarock@users.noreply.github.com>
2020-02-08 15:24:52 +1100

    Fibonacci methods
    two ways of generating fibonnaci numbers

---
+++ PythonLearning/temp.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+def fibRecursive(x):
+    if x == 0 :
+        return 0
+    elif x==1 :
+        return 1
+    else:
+        return fibRecursive(x-1) + fibRecursive(x-2)
+  
+
+def fibLinear(x):
+    fib = 0
+    for i in range(x+1):
+        if i == 0:
+            fib = 0
+        elif i ==1:
+            fib =1
+            fibl = 0
+        else:
+            fibtemp = fib
+            fib = fibtemp+ fibl
+            fibl = fibtemp
+    return fib
+    
+
+   
+for i in range(35):
+    print(fibLinear(i))
+    
+for i in range(35):
+    print(fibRecursive(i))
+ 
